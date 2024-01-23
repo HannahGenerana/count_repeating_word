bf8e4fd6973e6a5b2f787e6c3c8224dc42f8cc20
HannahGenerana <hannahgenerana@gmail.com>
2024-01-24 02:03:53 +0800

    pseudocode

---
+++ count_repeating_word.py
@@ -0,0 +1,10 @@
+# Exercise 7: Return the count of a given substring from a string
+# Write a program to find how many times substring “Peter” appears in the given string.
+
+# pseudocode
+
+# write a sentence
+
+# use the count method of a str class
+
+# print how many time the word is repeated
